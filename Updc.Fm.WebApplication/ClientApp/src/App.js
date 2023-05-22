608587f7999050866b98fb1e7a21040e9a3c99cb
Ojakovo Eric <edesiriojakovo@gmail.com>
2023-05-22 10:24:08 +0100

    updated sales

--- Updc.Fm.WebApplication/ClientApp/src/App.js
+++ Updc.Fm.WebApplication/ClientApp/src/App.js
@@ -1,34 +1,14 @@
-<<<<<<< HEAD
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import "./custom.css";
-import Login from "./Pages/Login";
-import Home from "./Pages/Home";
-import SalesNewUnit from "./components/SalesNewUnit";
-import PopBox from "./Utilities/PopBox";
-import SalesAllocation from "./components/SalesAllocation";
-import SalesView from "./components/SalesView";
-
-function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/sales" element={<PopBox/>} />
-      <Route path="/sales/unit" element={<SalesNewUnit />} />
-      <Route path="/sales/allocation" element={<SalesAllocation />} />
-      <Route path="/sales/view" element={<SalesView />} />
-
-    </Routes>
-=======
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './custom.css';
 import Login from './Pages/Login';
 import Home from './Pages/Home';
-import Sales from './Pages/Sales'
+import Sales from './Pages/Sales';
 import { ProtectedRoute } from './Auth/ProtectedRoute';
 import { AuthProvider } from './Auth/hooks/useAuth';
+import SalesAllocation from './components/SalesAllocation';
+import SalesView from './components/SalesView';
+import { SalesNewUnit } from './components/SalesNewUnit';
 
 function App() {
   return (
@@ -53,7 +33,6 @@ function App() {
         <Route path='/sales' element={<Sales />} />
       </Routes>
     </AuthProvider>
->>>>>>> c4a55db5d39723a63270cb75fe1251a5ec5196c5
   );
 }
 export default App;
