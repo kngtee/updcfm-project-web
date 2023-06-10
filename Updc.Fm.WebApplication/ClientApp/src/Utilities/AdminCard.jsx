5b2a79f20a287e2133aafca62115c3ebe96acf8b
AdebayoBunmiSolomon <61608290+AdebayoBunmiSolomon@users.noreply.github.com>
2023-06-10 21:33:05 +0100

    Adjusted changes

--- Updc.Fm.WebApplication/ClientApp/src/Utilities/AdminCard.jsx
+++ Updc.Fm.WebApplication/ClientApp/src/Utilities/AdminCard.jsx
@@ -1,37 +1,41 @@
 import React from 'react';
 
 const AdminCard = (props) => {
+  const truncateText = (str) => {
+    return str.length > 15 ? str.substring(0, 12) + '....' : str;
+  };
+
   return (
-    <div className="flex relative bg-white w-[240px] h-[120px] rounded-md shadow-sm shadow-[#a73439]/25">
+    <div className="flex flex-col space-y-1 relative bg-white w-[240px] h-[120px] rounded-md shadow-sm shadow-[#a73439]/25">
       <div className="flex absolute right-0 flex-row justify-end">
         <div>
           <input type="checkbox" />
         </div>
       </div>
-      <div className="flex flex-col justify-center items-center">
-        <div className="flex flex-row justify-center gap-20">
-          <div className="pl-5">
-            <h1 className="font-medium text-[#B9B0B0]">{props.Header}</h1>
-          </div>
-          <div>
-            <img
-              src={props.Icon}
-              alt="Real Estate"
-              className="h-[50px] w-[50px]"
-            />
-          </div>
+      {/* Estate name and Icon */}
+      <div className="flex flex-row justify-center space-x-20 pt-2">
+        <div>
+          <h1 className="font-medium text-[#B9B0B0]">{props.Header}</h1>
         </div>
-        <div className="flex flex-col justify-center pt-[-45rem] ml-[-3.5rem]">
-          <div className=" mt-[-1rem]">
-            <h1 className="font-medium text-[#0f0f0f] text-sm">
-              Name: {props.Name}
-            </h1>
-          </div>
-          <div>
-            <h1 className="font-medium text-[#0f0f0f] text-sm">
-              Manager: {props.Manager}
-            </h1>
-          </div>
+        <div>
+          <img
+            src={props.Icon}
+            alt="Estate Image"
+            className="h-[40px] w-[40px]"
+          />
+        </div>
+      </div>
+      {/* Name and title */}
+      <div className="flex flex-col">
+        <div className="pl-5">
+          <h1 className="font-medium text-[#0f0f0f] text-sm">
+            Name: {truncateText(props.Name)}
+          </h1>
+        </div>
+        <div className="pl-5">
+          <h1 className="font-medium text-[#0f0f0f] text-sm">
+            Manager: {truncateText(props.Manager)}
+          </h1>
         </div>
       </div>
     </div>
