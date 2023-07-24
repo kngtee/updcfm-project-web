d0a65a3d4383fa7c7737f3c1f2c81449a2240c21
ayinlaabel <kraneabel@gmail.com>
2023-07-24 10:21:00 +0100

    updated with single cluster and estates by cluster id

--- Updc.Fm.WebApplication/Controllers/ClustersController.cs
+++ Updc.Fm.WebApplication/Controllers/ClustersController.cs
@@ -47,5 +47,37 @@ namespace Updc.Fm.WebApplication.Controllers
 
             return StatusCode(400, response.Content.ReadAsStringAsync());
         }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetClusterById(string id)
+        {
+            var client = _httpClientFactory.CreateClient("api");
+            var response = await client.GetAsync($"/api/clusters/{id}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var res = await response.Content.ReadAsStringAsync();
+                return StatusCode(200, res);
+            }
+
+            return StatusCode(400, response.Content.ReadAsStringAsync());
+        }
+
+        [HttpGet]
+        [Route("{id}/estates")]
+        public async Task<IActionResult> GetEstateByClusterId(string id)
+        {
+            var client = _httpClientFactory.CreateClient("api");
+            var response = await client.GetAsync($"/api/clusters/{id}/estates");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var res = await response.Content.ReadAsStringAsync();
+                return StatusCode(200, res);
+            }
+
+            return StatusCode(400, response.Content.ReadAsStringAsync());
+        }
     }
 }
