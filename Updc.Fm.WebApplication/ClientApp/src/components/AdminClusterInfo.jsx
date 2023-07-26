81c93aa6240d82d65798f8de004e6c4a8742b104
AdebayoBunmiSolomon <solomon.bunmi@yahoo.com>
2023-07-26 13:55:30 +0100

    Admin cluster info

--- Updc.Fm.WebApplication/ClientApp/src/components/AdminClusterInfo.jsx
+++ Updc.Fm.WebApplication/ClientApp/src/components/AdminClusterInfo.jsx
@@ -6,6 +6,7 @@ import { errorMessage } from '../toast-message/toastMessage';
 import TableVariantAdminClusterInfo from '../Utilities/TableVariantAdminClusterInfo';
 import { useParams } from 'react-router-dom';
 import Table from '../Utilities/Table';
+import Loader from './Loader';
 
 let tableHeader = [
   { cluster_Id: 'Cluster Id' },
@@ -16,6 +17,7 @@ let tableHeader = [
 const AdminClusterInfo = () => {
   const [clusters, setClusters] = useState({});
   const [estates, setEstates] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const { id } = useParams();
 
   const truncateText = (str) => {
@@ -44,6 +46,7 @@ const AdminClusterInfo = () => {
         setEstates((i) => [...i, newE]);
       });
       console.log(estates);
+      setIsLoading(false);
     }
   };
 
@@ -54,99 +57,104 @@ const AdminClusterInfo = () => {
 
   return (
     <>
-      <NavContainer dashboard={adminClusterInfos}>
-        <div className="space-y-8 px-4 py-8">
-          <div className="flex flex-row" aria-label="Breadcrumb">
-            <ol className="inline-flex items-center space-x-1 md:space-x-2 font-normal">
-              <li className="items-center">
-                <a
-                  href="/admin"
-                  className="inline-flex items-center text-sm text-gray-500 hover:text-[#a73439]"
-                >
-                  Dashboard
-                </a>
-              </li>
-
-              <li class="inline-flex">
-                <div class="inline-flex items-center">
-                  <svg
-                    aria-hidden="true"
-                    fill="currentColor"
-                    className="w-3 h-3 text-gray-400"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                    stroke="currentColor"
-                    transform="rotate(160)"
-                  >
-                    <g id="SVGRepo_iconCarrier">
-                      <path d="M21.71,3.29a1,1,0,0,0-1.42,0l-18,18a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0l18-18A1,1,0,0,0,21.71,3.29Z"></path>
-                    </g>
-                  </svg>
-
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <NavContainer dashboard={adminClusterInfos}>
+          <div className="space-y-8 px-4 py-8">
+            <div className="flex flex-row" aria-label="Breadcrumb">
+              <ol className="inline-flex items-center space-x-1 md:space-x-2 font-normal">
+                <li className="items-center">
                   <a
-                    href="/admin/cluster"
-                    className="ml-1 items-center text-sm text-gray-500 hover:text-[#a73439] md:ml-2"
+                    href="/admin"
+                    className="inline-flex items-center text-sm text-gray-500 hover:text-[#a73439]"
                   >
-                    Cluster
+                    Dashboard
                   </a>
-                </div>
-              </li>
-              <li aria-current="page" class="inline-flex">
-                <div class="inline-flex items-center">
-                  <svg
-                    aria-hidden="true"
-                    fill="currentColor"
-                    className="w-3 h-3 text-gray-400"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                    stroke="currentColor"
-                    transform="rotate(160)"
-                  >
-                    <g id="SVGRepo_iconCarrier">
-                      <path d="M21.71,3.29a1,1,0,0,0-1.42,0l-18,18a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0l18-18A1,1,0,0,0,21.71,3.29Z"></path>
-                    </g>
-                  </svg>
-                  <span className="ml-1 text-sm text-[#d36360] md:ml-2">
-                    {clusters && truncateText(String(clusters.cluster_manager))}
-                  </span>
-                </div>
-              </li>
-            </ol>
-          </div>
-        </div>
-        <div className="flex flex-col md:flex-row md:space-x-12">
-          <div className="flex flex-col items-start justify-between px-6 py-4 w-fit bg-white rounded-md shadow-sm shadow-[#a73439]/25 md:flex-row md:w-[300px] md:max-h-[300px]">
-            <div className="text-xs font-medium justify-between leading-normal">
-              <p className="text-gray-400 text-sm">
-                {clusters && clusters.cluster_name}
-              </p>
-              <ol className="mt-1 text-[#0f0f0f]">
-                <li className="font-bold">
-                  Manager:
-                  <span className="font-medium">
-                    {' '}
-                    {clusters &&
-                      clusters.manager?.first_Name +
-                        ' ' +
-                        clusters.manager?.last_Name}
-                    {clusters && console.log(clusters)}
-                  </span>
+                </li>
+
+                <li class="inline-flex">
+                  <div class="inline-flex items-center">
+                    <svg
+                      aria-hidden="true"
+                      fill="currentColor"
+                      className="w-3 h-3 text-gray-400"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                      stroke="currentColor"
+                      transform="rotate(160)"
+                    >
+                      <g id="SVGRepo_iconCarrier">
+                        <path d="M21.71,3.29a1,1,0,0,0-1.42,0l-18,18a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0l18-18A1,1,0,0,0,21.71,3.29Z"></path>
+                      </g>
+                    </svg>
+
+                    <a
+                      href="/admin/cluster"
+                      className="ml-1 items-center text-sm text-gray-500 hover:text-[#a73439] md:ml-2"
+                    >
+                      Cluster
+                    </a>
+                  </div>
+                </li>
+                <li aria-current="page" class="inline-flex">
+                  <div class="inline-flex items-center">
+                    <svg
+                      aria-hidden="true"
+                      fill="currentColor"
+                      className="w-3 h-3 text-gray-400"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                      stroke="currentColor"
+                      transform="rotate(160)"
+                    >
+                      <g id="SVGRepo_iconCarrier">
+                        <path d="M21.71,3.29a1,1,0,0,0-1.42,0l-18,18a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0l18-18A1,1,0,0,0,21.71,3.29Z"></path>
+                      </g>
+                    </svg>
+                    <span className="ml-1 text-sm text-[#d36360] md:ml-2">
+                      {clusters &&
+                        truncateText(String(clusters.cluster_manager))}
+                    </span>
+                  </div>
                 </li>
               </ol>
             </div>
           </div>
-        </div>
-        <div className="mt-4">
-          {estates ? (
-            <TableVariantAdminClusterInfo
-              filter={['estate_Address', 'estate_Name']}
-              header={tableHeader}
-              data={estates && estates}
-              query={''}
-            />
-          ) : null}
-        </div>
-      </NavContainer>
+          <div className="flex flex-col md:flex-row md:space-x-12">
+            <div className="flex flex-col items-start justify-between px-6 py-4 w-fit bg-white rounded-md shadow-sm shadow-[#a73439]/25 md:flex-row md:w-[300px] md:max-h-[300px]">
+              <div className="text-xs font-medium justify-between leading-normal">
+                <p className="text-gray-400 text-sm">
+                  {clusters && clusters.cluster_name}
+                </p>
+                <ol className="mt-1 text-[#0f0f0f]">
+                  <li className="font-bold">
+                    Manager:
+                    <span className="font-medium">
+                      {' '}
+                      {clusters &&
+                        clusters.manager?.first_Name +
+                          ' ' +
+                          clusters.manager?.last_Name}
+                      {clusters && console.log(clusters)}
+                    </span>
+                  </li>
+                </ol>
+              </div>
+            </div>
+          </div>
+          <div className="mt-4">
+            {estates ? (
+              <TableVariantAdminClusterInfo
+                filter={['estate_Address', 'estate_Name']}
+                header={tableHeader}
+                data={estates && estates}
+                query={''}
+              />
+            ) : null}
+          </div>
+        </NavContainer>
+      )}
     </>
   );
 };
